6b3361af0a996dbb968beb688b766aef72e37fe0
Natan <pro.natansalvador@gmail.com>
2025-12-17 17:08:51 +0000

    wip

--- starshield/settings.py
+++ starshield/settings.py
@@ -186,6 +186,9 @@ class Base(Configuration):
     # URLS
     STATIC_URL = "static/"
     STATIC_ROOT = BASE_DIR / "staticfiles"
+    STATICFILES_DIRS = [
+        BASE_DIR / "static",
+    ]
 
     MEDIA_URL = "/media/"
     MEDIA_ROOT = BASE_DIR / "media"
