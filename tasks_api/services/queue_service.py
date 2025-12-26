36e28bf490530d46975075b2b699392211dce2c2
Natan <pro.natansalvador@gmail.com>
2025-12-26 21:26:24 +0000

    wip

--- tasks_api/services/queue_service.py
+++ tasks_api/services/queue_service.py
@@ -16,7 +16,7 @@ from auths.models import Etablissement
 logger = logging.getLogger(__name__)
 
 
-def create_google_cloud_task(
+async def create_google_cloud_task(
     queue: str,
     url: str,
     payload: dict,
@@ -57,7 +57,7 @@ def create_google_cloud_task(
         task.dispatch_deadline = duration
 
     # Create the task
-    client.create_task(
+    await client.create_task(
         tasks_v2.CreateTaskRequest(
             parent=client.queue_path(PROJECT_ID, LOCATION, queue),
             task=task,
