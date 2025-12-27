483ef9417be6c8bf0f05b2706c8a376adda9ead8
Natan <pro.natansalvador@gmail.com>
2025-12-27 19:36:35 +0000

    wip

--- frontend/dashboard/templatetags/filters.py
+++ frontend/dashboard/templatetags/filters.py
@@ -13,9 +13,14 @@ def after_original(value):
     if not value:
         return value
 
+    kept_text = value
+
     if "(Original)" in value:
         parts = value.split("(Original)", 1)
         if len(parts) > 1:
-            return parts[1].strip()
+            kept_text = parts[1].strip()
 
-    return value
+    if "(Translated by Google)" in value:
+        kept_text = kept_text.split("(Translated by Google)", 1)[0].strip()
+
+    return kept_text
