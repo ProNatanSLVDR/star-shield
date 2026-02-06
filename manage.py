7185c5913f05dfe5e6d993fa33f11fc4a9339a31
Natan <pro.natansalvador@gmail.com>
2026-02-06 21:45:21 +0000

    wip

--- manage.py
+++ manage.py
@@ -2,6 +2,11 @@
 
 import os
 import sys
+from pathlib import Path
+
+from dotenv import load_dotenv
+
+load_dotenv(Path(__file__).resolve().parent / ".devcontainer" / "dev.env")
 
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "starshield.settings")
